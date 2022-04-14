815d29ec781cec6ad8daa6819da739769b905a98
Rhiannon Jones <rjones0211@hotmail.com>
2022-04-14 18:42:16 +0100

    Added the analysis macro and utilities for comparing productions, nothing in it yet

---
+++ srcs/MultiProductionAnalysis.cpp
@@ -0,0 +1,222 @@
+/************************************************************************
+ * 
+ * A macro to understand the truth-level hit and track distributions for 
+ * dE/dx calibration studies
+ *
+ *
+ * Example file list located here:
+ *   /home/jones/work/cosmics/LArSoft-v08_50_00/work/files/anafiles.list
+ *
+ *
+ *************************************************************************/
+
+#include "EventProcessor.h"
+#include "ConfigReader.h"
+
+using namespace calib;
+using namespace cppsecrets;
+
+// Allowed branches to read from the tree
+std::vector<TString> allowed = {
+   "run",
+   "event",
+   "geant_list_size",
+   "inTPCActive",
+   "TrackId",
+   "pdg",
+   "Mother",
+   "ntracks_pandoraTrack",
+   "trkId_pandoraTrack",
+   "trkidtruth_pandoraTrack",
+   "trkpdgtruth_pandoraTrack",
+   "trkg4id_pandoraTrack",
+   "ntrkhits_pandoraTrack",
+   "trkresrg_pandoraTrack",
+   "trkdqdx_pandoraTrack",
+   "trkdedx_pandoraTrack",
+   "trkke_pandoraTrack",
+   "trkxyz_pandoraTrack",
+   "trkstartx_pandoraTrack",
+   "trkstarty_pandoraTrack",
+   "trkstartz_pandoraTrack",
+   "trkendx_pandoraTrack",
+   "trkendy_pandoraTrack",
+   "trkendz_pandoraTrack",
+   "trklen_pandoraTrack",
+   "no_hits_stored",
+   "hit_tpc",
+   "hit_plane",
+   "hit_charge",
+   "hit_energy",
+   "hit_nelec",
+   "hit_trueX",
+   "hit_trkid",
+   "hit_peakT",
+   "hit_startT",
+   "hit_endT",
+   "StartPointx",
+   "StartPointy",
+   "StartPointz",
+   "EndPointx",
+   "EndPointy",
+   "EndPointz",
+   "StartPointx_tpcAV",
+   "StartPointy_tpcAV",
+   "StartPointz_tpcAV",
+   "EndPointx_tpcAV",
+   "EndPointy_tpcAV",
+   "EndPointz_tpcAV",
+   "NumberDaughters",
+   "P",
+   "Eng",
+   "EndE",
+   "pathlen"
+ };
+
+// A translation list from plane labels to longer labels for plotting
+std::map<std::string, std::string> planeLabels = {
+  {"h0", "APA 1"},
+  {"h1", "CPA 1"},
+  {"h2", "APA 2"},
+  {"h3", "CPA 2"},
+  {"h4", "APA 3"},
+  {"t",  "Top"},
+  {"bo", "Bot."},
+  {"f",  "Fro."},
+  {"ba", "Back"},
+};
+
+typedef std::vector<Plane> PlaneList;
+     
+int multiProductionAnalysis(const char *config){
+
+  // First, setup timing information so we can monitor the run
+  time_t rawtime;
+  std::cout << "-----------------------------------------------------------" << std::endl;
+  GetTime(rawtime);
+  std::cout << "-----------------------------------------------------------" << std::endl;
+
+  //------------------------------------------------------------------------------------------
+  //                                    Configure
+  //------------------------------------------------------------------------------------------
+  // Create object of the class ConfigReader
+  // Parse the configuration file
+  // Dump map on the console after parsing it
+  ConfigReader* p = ConfigReader::getInstance();
+  p->parseFile(config);
+  std::cout << " Variables from configuration file: " << std::endl;
+  p->dumpFileValues();
+  std::cout << "-----------------------------------------------------------" << std::endl;
+
+  // Get configuration variables
+  int n = -1;
+  std::string input_list = "";
+  std::string location="";
+  std::string tag="";
+  std::vector<double> minx_fid, miny_fid, minz_fid;
+  std::vector<double> maxx_fid, maxy_fid, maxz_fid;
+  std::vector<double> minx_av, miny_av, minz_av;
+  std::vector<double> maxx_av, maxy_av, maxz_av;
+
+  p->getValue("InputList", input_list);
+  p->getValue("Location",  location);
+  p->getValue("Tag",       tag);
+  p->getValue("NFiles",    n);
+  p->getValue("MinXFid",   minx_fid);
+  p->getValue("MinYFid",   miny_fid);
+  p->getValue("MinZFid",   minz_fid);
+  p->getValue("MaxXFid",   maxx_fid);
+  p->getValue("MaxYFid",   maxy_fid);
+  p->getValue("MaxZFid",   maxz_fid);
+  p->getValue("MinXAV",    minx_av);
+  p->getValue("MinYAV",    miny_av);
+  p->getValue("MinZAV",    minz_av);
+  p->getValue("MaxXAV",    maxx_av);
+  p->getValue("MaxYAV",    maxy_av);
+  p->getValue("MaxZAV",    maxz_av);
+
+  // Get the active and fiducial geometry objects
+  Geometry fiducial(minx_fid,miny_fid,minz_fid,maxx_fid,maxy_fid,maxz_fid,true);
+  Geometry active(minx_av,miny_av,minz_av,maxx_av,maxy_av,maxz_av,false);
+  PlaneList extPlanes = active.GetExternalPlaneList();
+  PlaneList allPlanes = active.GetPlaneList();
+  PlaneList intPlanes = active.GetInternalPlaneList(allPlanes,extPlanes);
+  PlaneList fidExtPlanes = fiducial.GetExternalPlaneList();
+  PlaneList fidAllPlanes = fiducial.GetPlaneList();
+
+  std::cout << "-----------------------------------------------------------" << std::endl;
+  std::cout << " Total number of planes in the active volume of the DUNE SP module: " << allPlanes.size() << std::endl;
+  std::cout << " Consisting of " << extPlanes.size() << " external planes and " << intPlanes.size() << " internal planes" << std::endl; 
+  std::cout << "-----------------------------------------------------------" << std::endl;
+ 
+  // Sort out the file tag
+  if(tag != "")
+    tag = "_"+tag;
+
+  //--------------------------------------------------------------------------------- ---------
+  //                                    Initialise
+  //--------------------------------------------------------------------------------- ---------
+
+  // Setup vectors for each TTree and set of labels for the productions
+  std::vector<TChain*> prodTrees;
+  std::vector<anatree*> prodEvents;
+  std::vector<std::string> prodFiles, prodLabels, prodTeXLabels;
+
+  ReadCSV(input_list, n, prodFiles, prodLabels, prodTeXLabels);
+
+  // In the case of the files themselves, pass to the EventProcessor before filling the anatree vector
+  // Setup TTrees from input file list
+  std::cout << " Reading files and filling tree" << std::endl;
+
+  unsigned int f = 0;
+  for(const std::string &file : prodFiles){
+    EventProcessor evtProc(allowed, file, n);
+    evtProc.Initialize(false);
+
+    // Now setup the tree and event objects to work with
+    TChain *tree = evtProc.GetTree();
+    anatree *evt = evtProc.GetEvents();
+    prodTrees.push_back(tree);
+    prodEvents.push_back(evt);
+
+    // Start of analysis (loop over chain and events
+    std::cout << " Running analysis over file " << file << "..." << std::endl;
+
+    // Then setup the histograms, counters and any other variables to add to
+    // Setup histograms
+
+    // Setup counters
+
+    // Now loop over the events
+    unsigned int nEvts = tree->GetEntries();
+    unsigned int iIt = 1;
+
+    std::cout << " |";
+    for(unsigned int iEvt = 0; iEvt < nEvts; ++iEvt){
+      tree->GetEntry(iEvt);
+      if(!evtProc.SelectEvent(evt)) continue;
+
+      // Print the processing rate
+      double evtFrac  = iEvt/static_cast<double>(nEvts);
+
+      if(std::abs(0.1*iIt-evtFrac) < std::numeric_limits<double>::epsilon()){
+        std::cout << " --- " << evtFrac*100 << " %";
+        std::cout.flush();
+        iIt++;
+      }
+    }// Event loop
+    f++;
+  }// File loop
+  std::cout << " --- 100 % --- |" << std::endl;
+
+  // End of script
+  std::cout << " ...finished analysis" << std::endl;
+  std::cout << "-----------------------------------------------------------" << std::endl;
+  time_t rawtime_end;
+  GetTime(rawtime_end);
+  std::cout << "-----------------------------------------------------------" << std::endl;
+  GetTotalTime(rawtime, rawtime_end);
+  std::cout << "-----------------------------------------------------------" << std::endl;
+  
+  return 0;
+}
