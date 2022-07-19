c0430d4e9f033e8850892b56bc67295023ce93de
Rhiannon Jones <rjones0211@hotmail.com>
2022-07-19 13:04:47 +0100

    Moved run scripts into a directory to clean up the repo

---
+++ run/run_DependenceRemovalStudies.C
@@ -0,0 +1,14 @@
+{
+  gROOT->ProcessLine(".L $RHIANA/srcs/Setup.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/Plane.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/Geometry.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/PlottingHelpers.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/GeometryHelpers.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/SliceHelpers.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/AnalysisHelpers.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/ExternalFunctionHelpers.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/ReadFiles.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/ConfigReader.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/EventProcessor.cpp+");
+  gROOT->ProcessLine(".L $RHIANA/srcs/DependenceRemovalStudies.cpp+");
+}
